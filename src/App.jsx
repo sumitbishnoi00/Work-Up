a57cd0dba69331485fd558d024f1fe86fd0970c9
pawandelu <pawandelu08@gmail.com>
2026-06-22 15:00:49 +0530

    tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import './App.css'
-import Footer from './components/common/Footer'
-import Navbar from './components/common/Navbar'
-import CardSwiper from './components/home/CardSwiper'
-import Hero from './components/home/Hero'
+import "./App.css";
+import Footer from "./components/common/Footer";
+import CardSwiper from "./components/home/CardSwiper";
+import Hero from "./components/home/Hero";
 
 function App() {
-  
-
   return (
     <>
-     <Navbar />
-     <Hero />
-     <CardSwiper />
-     <Footer />
+      <Hero />
+      <CardSwiper />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
