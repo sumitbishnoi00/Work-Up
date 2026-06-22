5cb0d405912a28bb7e5d1f98317c0f622cf951db
sumitbishnoi00 <sumitbishnoi369@gmail.com>
2026-06-22 14:45:07 +0530

    add section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Footer from './components/common/Footer'
+import Navbar from './components/common/Navbar'
+import CardSwiper from './components/home/CardSwiper'
+import Hero from './components/home/Hero'
 
 function App() {
   
 
   return (
     <>
-     
+     <Navbar />
+     <Hero />
+     <CardSwiper />
+     <Footer />
     </>
   )
 }
